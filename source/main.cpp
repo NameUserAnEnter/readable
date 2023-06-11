299978d4b2663e61d91a379abd11b9b6ffd04417
NameUserAnEnter <negativeman55@gmail.com>
2023-06-11 22:20:10 +0200

    Imported methods from project24.

---
+++ source/main.cpp
@@ -0,0 +1,180 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+std::string EncodeToUTF8(std::wstring str)
+{
+	std::string encoded = "";
+	for (int i = 0; i < str.size(); i++)
+	{
+		wchar_t character = str[i];
+		if (character <= 0x7F) encoded += (unsigned char)character;
+		else
+		{
+			if (character >= 0x80 && character <= 0x7FF)
+			{
+				encoded += (unsigned char)(((0xC0 << 8) + ((character << 2) & (0x1F << 8))) >> 8);
+				encoded += (unsigned char)(((0x80) + (character & 0x3F)));
+			}
+			else if (character >= 0x800 && character <= 0xFFFF)
+			{
+				encoded += (unsigned char)(((0xE0 << 16) + ((character << 4) & (0x0F << 16))) >> 16);
+				encoded += (unsigned char)(((0x80 << 8) + ((character << 2) & (0x3F << 8))) >> 8);
+				encoded += (unsigned char)(((0x80) + (character & 0x3F)));
+			}
+			else if (character >= 0x10000)
+			{
+				// wchar_t is 16-bit on Windows, 0x10000+ code points are not supported
+			}
+		}
+	}
+	return encoded;
+}
+
+std::wstring DecodeFromUTF8(std::string str)
+{
+	std::wstring decoded = L"";
+	for (int i = 0; i < str.size(); i++)
+	{
+		wchar_t currentChar = str[i];
+		if ((currentChar & 128) == 128 && (currentChar & 64) == 64)
+		{
+			if ((currentChar & 32) == 0 && i + 1 < str.size())
+			{
+				// 2-Byte encoded character
+				// 110xxxxx ...
+				unsigned char Byte[] ={ (unsigned char)currentChar & 31, str[i + 1] & 63 };
+				i++;
+
+				// 16-bit decoded character:
+				currentChar = (Byte[1]) + (((unsigned short)Byte[0] << 8) >> 2);
+			}
+			else if ((currentChar & 32) == 32)
+			{
+				if ((currentChar & 16) == 0 && i + 2 < str.size())
+				{
+					// 3-Byte encoded character
+					// 1110xxxx ...
+					unsigned char Byte[] ={ (unsigned char)currentChar & 15, str[i + 1] & 63, str[i + 2] & 63 };
+					i++;
+					i++;
+
+					// 16-bit decoded character:
+					currentChar = (Byte[2]) + (((unsigned short)Byte[1] << 8) >> 2) + (((unsigned int)Byte[0] << 16) >> 4);
+				}
+				else if ((currentChar & 16) == 16 && i + 3 < str.size())
+				{
+					if ((currentChar & 8) == 0)
+					{
+						//4-Byte encoded character
+						// 11110xxx ...
+						for (int i = 0; i < 3; i++) i++;
+
+						// 16-bit substitue character:
+						currentChar = 'e';
+					}
+				}
+			}
+		}
+
+		decoded += currentChar;
+	}
+	return decoded;
+}
+
+void PrintConsole(std::wstring output)
+{
+	printf("%ls", output.c_str());
+}
+
+void PrintConsole(std::string output)
+{
+	printf("%s", output.c_str());
+}
+
+int GetFileData(const wchar_t* filename, std::string* returnData = nullptr)
+{
+	std::string data = "";
+
+	FILE* file;
+	_wfopen_s(&file, filename, L"rb");
+	if (file == nullptr)
+	{
+		PrintConsole(L"Cannot open: " + std::wstring(filename) + L'\n');
+		return 1;
+	}
+
+	int currentChar;
+	do
+	{
+		currentChar = getc(file);
+		if (currentChar != EOF) data += (unsigned char)currentChar;
+	} while (currentChar != EOF);
+	fclose(file);
+
+	if (returnData != nullptr) *returnData = data;
+	return 0;
+}
+
+int WriteDataToFile(std::string data, const wchar_t* filename)
+{
+	FILE* file;
+	_wfopen_s(&file, filename, L"wb");
+	if (file == nullptr)
+	{
+		PrintConsole(L"Cannot open: " + std::wstring(filename) + L'\n');
+		return 1;
+	}
+
+	for (int i = 0; i < data.size(); i++)
+	{
+		putc((unsigned char)data[i], file);
+	}
+	fclose(file);
+
+	return 0;
+}
+
+void ClearFile(std::wstring filename)
+{
+	std::string empty_data = "";
+	WriteDataToFile(empty_data, filename.c_str());
+}
+
+
+
+void modifyData(std::string* data)
+{
+	if (data == nullptr) return;
+	std::string copy = *data;
+
+
+	int nest = 0;
+	for (int i = 0; i < copy.size(); i++)
+	{
+		if (copy[i] == '{')
+		{
+			nest++;
+		}
+
+		// replace()
+	}
+
+
+	*data = copy;
+}
+
+
+int main()
+{
+	std::string data = "";
+	GetFileData(L"D:/conversion/tbdl/parsed_regex_sample.txt", &data);
+
+	modifyData(&data);
+	PrintConsole(data);
+
+	//
+
+	while (getc(stdin) != 'x') { };
+	return 0;
+}
