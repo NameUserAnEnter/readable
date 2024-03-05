58907bf8a26d9738cad27252488d7d84a084ce72
NameUserAnEnter <negativeman55@gmail.com>
2024-03-05 18:02:53 +0100

    Implemented automatic mode deduction, improved output filenames by adding an appendix in case of collision with an existing file. Stripped down utils.h from unused functions.

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <string>
-#include <vector>
 #include "utils.h"
 #include "console.h"
-#include "parse.h"
+#include "readable.h"
 
 
 int main(int argc, char* argv[])
@@ -16,6 +13,9 @@ int main(int argc, char* argv[])
 	std::string inputFile;
 	std::string outputFile;
 
+	// -------------------------------------------------------
+	
+	// Parse args if any have been passed.
 	if (argc > 0)
 	{
 		for (int i = 0; i < argc; i++)
@@ -54,64 +54,30 @@ int main(int argc, char* argv[])
 		}
 	}
 
-	while (mode == def)
-	{
-		PrintConsole("Mode? (json/html/help): ");
-		std::string response = ReadConsole();
-
-		if (response == "json")
-		{
-			mode = json;
-			break;
-		}
-		if (response == "html")
-		{
-			mode = html;
-			break;
-		}
-		if (response == "help")
-		{
-			mode = help;
-			break;
-		}
-	}
-
-	if (mode == help)
-	{
-		PrintConsole("\nUSAGE:\n");
-		PrintConsole("readable[.exe] [ARGS] [INPUT] [OUTPUT]\n");
-
-		PrintConsole("\nARGS:\n");
-		PrintConsole("-h, --html: Use HTML mode.\n");
-		PrintConsole("-j, --json: Use JSON mode.\n");
-		PrintConsole("--help: Print help.\n");
-
-		PrintConsole("\nINPUT:\n");
-		PrintConsole("Existing input file.\n");
-
-		PrintConsole("\nOUTPUT:\n");
-		PrintConsole("Output file name.\n");
-
-		PrintConsole("\n");
-
-		getc(stdin);
-		return 0;
-	}
-
 	// -------------------------------------------------------
-
+	
+	// If not provided, ask for input file.
 	if (!knowInput)
 	{
 		PrintConsole("Input file: ");
 		inputFile = ReadConsole();
 	}
+	if (!FileExist(toWide(inputFile).c_str()))
+	{
+		PrintConsole("\nFailed to open file '" + inputFile + "'.\n");
+
+		PrintConsole("Press ENTER to continue: ");
+		ReadConsole();
+		return 1;
+	}
+
+
 	if (!knowOutput)
 	{
-		// outputFile is set automatically to:
+		// Attempt to set the output filename to:
 		// inputFileBasename + '_readable' + inputFileExtension
 		//
 		// (where inputFileExtension is dot-inclusive)
-
 
 		outputFile = "";
 		std::string basename = "";
@@ -137,16 +103,91 @@ int main(int argc, char* argv[])
 		}
 
 		outputFile = basename + "_readable" + ext;
+
+		// To avoid collision with existing files, attempt to add an appendix number to the basename.
+		unsigned long appendix = 1;
+		while (FileExist(toWide(outputFile).c_str()))
+		{
+			// Ask user for output filename only if the maximum possible appendix number is reached and still colliding.
+			if (appendix >= 0xFFFFFFFF)
+			{
+				PrintConsole("Output file: ");
+				outputFile = ReadConsole();
+
+				break;
+			}
+
+			appendix++;
+			outputFile = basename + "_readable" + NumToStr(appendix) + ext;
+		}
+
+		// Attempt to automatically deduce mode from input file extension.
+		// If not deduced ask the user for mode.
+		if (ext == ".js" || ext == ".json") mode = json;
+		else if (ext == ".html") mode = html;
+		else
+		{
+			while (mode == def)
+			{
+				PrintConsole("Mode? (json/html/help): ");
+				std::string response = ReadConsole();
+
+				if (response == "json")
+				{
+					mode = json;
+					break;
+				}
+				if (response == "html")
+				{
+					mode = html;
+					break;
+				}
+				if (response == "help")
+				{
+					mode = help;
+					break;
+				}
+			}
+		}
 	}
 
 	// -------------------------------------------------------
+	// help mode
+
+	if (mode == help)
+	{
+		PrintConsole("\nUSAGE:\n");
+		PrintConsole("readable[.exe] [ARGS] [INPUT] [OUTPUT]\n");
+
+		PrintConsole("\nARGS:\n");
+		PrintConsole("-h, --html: Use HTML mode.\n");
+		PrintConsole("-j, --json: Use JSON mode.\n");
+		PrintConsole("--help: Print help.\n");
+		PrintConsole("\nPass no args to run in interactive mode.\n");
+
+		PrintConsole("\nINPUT:\n");
+		PrintConsole("Existing input file.\n");
+
+		PrintConsole("\nOUTPUT:\n");
+		PrintConsole("Output file name.\n");
+
+		PrintConsole("\n");
+
+		PrintConsole("Press ENTER to continue: ");
+		ReadConsole();
+		return 0;
+	}
+
+	// -------------------------------------------------------
+	// standard modes
 
 	std::string data = "";
 	if (GetFileData(toWide(inputFile).c_str(), &data))
 	{
-		PrintConsole("Failed to open file '" + inputFile + "'.\n");
+		PrintConsole("\nFailed to open file '" + inputFile + "'.\n");
 
-		getc(stdin);
+		PrintConsole("Press ENTER to continue: ");
+		ReadConsole();
 		return 1;
 	}
 
@@ -154,12 +195,11 @@ int main(int argc, char* argv[])
 	if (mode == json) ReadableJSON(&data);
 	PrintConsole(data);
 
-	ClearFileData(toWide(outputFile).c_str());
 	WriteDataToFile(data, toWide(outputFile).c_str());
-
 	PrintConsole("\nWrote " + std::to_string(data.size()) + " characters to '" + outputFile + "'.\n");
 
-	getc(stdin);
+	PrintConsole("Press ENTER to continue: ");
+	ReadConsole();
 	return 0;
 }
 
--- source/utils.h
+++ source/utils.h
@@ -2,10 +2,7 @@
 #define UTILS_H
 
 #include <string>
-#include <vector>
-#include <windows.h>
-#include <thread>
-#include <time.h>
+
 
 inline double power(double base, unsigned int exponent)
 {
@@ -55,10 +52,6 @@ inline std::string HexToStr(unsigned long long hex, unsigned int digits = 2)
 
 
 
-
-
-
-
 inline std::wstring toWide(std::string str)
 {
     std::wstring wide;
@@ -84,123 +77,6 @@ inline std::string fromWide(std::wstring wstr)
 
 
 
-inline void MessageDialog(std::wstring msg, std::wstring title = L"")
-{
-    if (!msg.empty())
-    {
-        if (msg.back() == L'\n') msg.pop_back();
-    }
-
-    MessageBoxW(NULL, msg.c_str(), title.c_str(), MB_OK);
-}
-
-inline void MessageDialog(std::string msg, std::string title = "")
-{
-    MessageDialog(toWide(msg), toWide(title));
-}
-
-
-
-inline std::string FuncErrOutput(std::string function)
-{
-    std::string output = "Failed to invoke " + function + "()";
-    return output;
-}
-
-inline std::wstring FuncErrOutput(std::wstring function)
-{
-    std::wstring output = L"Failed to invoke " + function + L"()";
-    return output;
-}
-
-inline std::string FuncErrOutput(std::string function, unsigned long external_code)
-{
-    std::string output = FuncErrOutput(function);
-    output += ": " + NumToStr(external_code) + " (" + HexToStr(external_code) + ")";
-
-    return output;
-}
-
-inline std::wstring FuncErrOutput(std::wstring function, unsigned long external_code)
-{
-    std::wstring output = FuncErrOutput(function);
-    output += L": " + toWide(NumToStr(external_code)) + L" (" + toWide(HexToStr(external_code)) + L")";
-
-    return output;
-}
-
-inline void ErrFuncExit(std::string function)
-{
-    std::string output = FuncErrOutput(function);
-
-    MessageDialog(output);
-    ExitProcess(0x01);
-}
-
-inline void ErrFuncExitCode(std::string function, unsigned long external_code)
-{
-    std::string output = FuncErrOutput(function, external_code);
-
-    MessageDialog(output);
-    ExitProcess(0x01);
-}
-
-inline void ErrFuncExit(std::wstring function)
-{
-    std::wstring output = FuncErrOutput(function);
-
-    MessageDialog(output);
-    ExitProcess(0x01);
-}
-
-inline void ErrFuncExitCode(std::wstring function, unsigned long external_code)
-{
-    std::wstring output = FuncErrOutput(function, external_code);
-
-    MessageDialog(output);
-    ExitProcess(0x01);
-}
-
-
-inline void ErrMsgExit(std::string msg)
-{
-    MessageDialog(msg);
-    ExitProcess(0x01);
-}
-
-inline void ErrMsgExitCode(std::string msg, unsigned long external_code)
-{
-    MessageDialog(msg);
-    ExitProcess(0x01);
-}
-
-inline void ErrMsgExit(std::wstring msg)
-{
-    MessageDialog(msg);
-    ExitProcess(0x01);
-}
-
-inline void ErrMsgExitCode(std::wstring msg, unsigned long external_code)
-{
-    MessageDialog(msg);
-    ExitProcess(0x01);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 inline int GetFileData(const wchar_t* filename, std::string* returnData = nullptr)
 {
     std::string data = "";
@@ -209,7 +85,6 @@ inline int GetFileData(const wchar_t* filename, std::string* returnData = nullpt
     _wfopen_s(&file, filename, L"rb");
     if (file == nullptr)
     {
-        //ErrMsgExit(L"Cannot open: " + std::wstring(filename) + L'\n');
         return 1;
     }
 
@@ -231,7 +106,6 @@ inline int WriteDataToFile(std::string data, const wchar_t* filename)
     _wfopen_s(&file, filename, L"wb");
     if (file == nullptr)
     {
-        //ErrMsgExit(L"Cannot open: " + std::wstring(filename) + L'\n');
         return 1;
     }
 
@@ -268,82 +142,22 @@ inline int ClearFileData(const wchar_t* filename)
     return WriteDataToFile("", filename);
 }
 
-
-
-inline std::wstring GetBackslashPath(std::wstring forwardslashPath)
+inline bool FileExist(const wchar_t* filename)
 {
-    std::wstring backslashPath = L"";
-    for (int i = 0; i < forwardslashPath.size(); i++)
+    FILE* file;
+    _wfopen_s(&file, filename, L"rb");
+    if (file == nullptr)
     {
-        if (forwardslashPath[i] != '/')
-        {
-            backslashPath += forwardslashPath[i];
-        }
-        else if (forwardslashPath[i] == '/' && i != forwardslashPath.size() - 1)
-        {
-            backslashPath += L"\\";
-        }
+        return false;
     }
-    return backslashPath;
-}
 
-
-template<typename T>
-inline bool findCharInStr(std::basic_string<T> str, T query)
-{
-    for (int i = 0; i < str.size(); i++)
-    {
-        if (str[i] == query) return true;
-    }
-    return false;
-}
-
-
-inline bool isStrNum(std::basic_string<wchar_t> str)
-{
-    for (auto symbol : str)
-    {
-        if (!iswdigit(symbol))
-        {
-            return false;
-        }
-    }
+    fclose(file);
     return true;
 }
 
-inline bool isStrNum(std::basic_string<char> str)
-{
-    for (auto symbol : str)
-    {
-        if (!isdigit(symbol))
-        {
-            return false;
-        }
-    }
-    return true;
-}
 
-template<typename T>
-inline bool findInStr(std::basic_string<T> str, std::basic_string<T> query)
-{
-    unsigned int index = 0;
-    for (int i = 0; i < str.size(); i++)
-    {
-        if (str[i] == query[index])
-        {
-            index++;
-            if (index == query.size())
-            {
-                return true;
-            }
-        }
-        else
-        {
-            index = 0;
-        }
-    }
-    return false;
-}
+
+
 
 
 template<typename T>
@@ -366,38 +180,6 @@ inline int whereSubStr(std::basic_string<T> str, std::basic_string<T> query)
 }
 
 template<typename T>
-inline void replaceSubStr(std::basic_string<T>& str, std::basic_string<T> query, std::basic_string<T> replacement)
-{
-    int start = whereSubStr(str, query);
-    std::basic_string<T> copy;
-    copy.clear();
-
-    for (int i = 0; i < start; i++)
-    {
-        copy += str[i];
-    }
-    for (int i = 0; i < replacement.size(); i++)
-    {
-        copy += replacement[i];
-    }
-    for (int i = start + query.size(); i < str.size(); i++)
-    {
-        copy += str[i];
-    }
-
-    str = copy;
-}
-
-template<typename T>
-inline void replaceAllSubStr(std::basic_string<T>& str, std::basic_string<T> query, std::basic_string<T> replacement)
-{
-    while (whereSubStr(str, query) != -1)
-    {
-        replaceSubStr(str, query, replacement);
-    }
-}
-
-template<typename T>
 inline bool beginWith(std::basic_string<T> str, std::basic_string<T> query)
 {
     if (whereSubStr(str, query) == 0) return true;
@@ -410,74 +192,8 @@ inline bool beginWith(std::basic_string<T> str, const T* query)
     return beginWith(str, std::basic_string<T>(query));
 }
 
-template<typename T>
-inline std::vector<std::basic_string<T>> splitByChar(std::basic_string<T> str, T query, bool leaveQueried = true)
-{
-    std::vector<std::basic_string<T>> returnValue;
-    returnValue.clear();
-    returnValue.push_back(std::basic_string<T>());
-    for (int i = 0; i < str.size(); i++)
-    {
-        returnValue.back() += str[i];
-        if (str[i] == query)
-        {
-            if (!leaveQueried) returnValue.back().pop_back();
-            returnValue.push_back(std::basic_string<T>());
-        }
-    }
-
-    if (!returnValue.empty())
-    {
-        if (returnValue.back().size() == 0) returnValue.pop_back();
-    }
-    return returnValue;
-}
 
 
-
-
-
-
-
-
-
-
-
-inline std::tm GetCurrentDateAndTime()
-{
-    std::time_t seconds_since = time(nullptr);
-    std::tm calendar = { };
-    gmtime_s(&calendar, &seconds_since);
-
-    calendar.tm_year += 1900;
-    calendar.tm_mon += 1;
-
-    return calendar;
-}
-
-inline std::string GetDateAndTimeStr()
-{
-    std::tm current_time = GetCurrentDateAndTime();
-    unsigned int mt = current_time.tm_mon;
-    unsigned int dd = current_time.tm_mday;
-    unsigned int yyyy = current_time.tm_year;
-    unsigned int hh = current_time.tm_hour;
-    unsigned int mn = current_time.tm_min;
-    unsigned int ss = current_time.tm_sec;
- 
-    std::string str = "";
-    str += "[";
-    str += NumToStr(mt, 10, 2) + "-";
-    str += NumToStr(dd, 10, 2) + "-";
-    str += NumToStr(yyyy, 10, 4) + ": ";
-
-    str += NumToStr(hh, 10, 2) + ":";
-    str += NumToStr(mn, 10, 2) + ":";
-    str += NumToStr(ss, 10, 2);
-    str += " UTC+0]";
-
-    return str;
-}
 
 
 #endif
